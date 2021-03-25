c0a591ff0870cc7999e7b0dbba90a4c59f23abd3
Ishara Pathirage <ihpathirage@virtusa.com>
2021-03-25 18:15:24 +0530

    0.24.0

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 resource "aws_instance" "example" {
-  ami= lookup({a="ami-830c94e3", b="ami-830c94e3ssss"}, "a", lower("ami-830c94e3"))
+  ami           = lookup({ a = "ami-830c94e3", b = "ami-830c94e3ssss" }, "a", lower("ami-830c94e3"))
   instance_type = "t2.micro"
-  tags = var.tags
+  tags          = var.tags
 }
 
 
 variable "tags" {
   type = map(string)
   default = {
-    env:"prod"
-    name:"ec2"
-    tagone:"ec2"
+    env : "prod"
+    name : "ec2"
+    tagone : "ec2"
   }
 }
